76856d93afe2f43befc8d6fafea22479cc7e9c92
patryk77k <patryk77.k@gmail.com>
2023-05-03 21:12:50 +0200

    topbar component added

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./topbar.css";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import LanguageIcon from "@mui/icons-material/Language";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 export const Topbar = () => {
   return (
@@ -12,8 +14,20 @@ export const Topbar = () => {
         <div className="topRight">
           <div className="topbarIconContainer">
             <NotificationsNoneIcon />
-            <span className="topIconBag">2</span>
+            <span className="topIconBadge">2</span>
           </div>
+          <div className="topbarIconContainer">
+            <LanguageIcon />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <SettingsIcon />
+          </div>
+          <img
+            src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+            className="topAvatar"
+          />
         </div>
       </div>
     </div>
