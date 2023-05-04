9720b619f24e83fe31c5f10e63ca83cd5a65d3f9
patryk77k <patryk77.k@gmail.com>
2023-05-04 12:19:52 +0200

    featuredInfo ready

---
+++ src/components/featuredInfo/FeaturedInfo.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./featuredInfo.css";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+
+const FeaturedInfo = () => {
+  return (
+    <div className="featured">
+      <div className="featuredItem">
+        <span className="featuredTitle">Revanue</span>
+        <div className="featuredMoneyContainer">
+          <span className="featuredMoney">$2,415</span>
+          <span className="featuredMoneyRate">
+            -11.4
+            <ArrowDownwardIcon />
+          </span>
+        </div>
+        <span className="featuredSub">Compare to last month</span>
+      </div>
+      <div className="featuredItem">
+        <span className="featuredTitle">Revanue</span>
+        <div className="featuredMoneyContainer">
+          <span className="featuredMoney">$2,415</span>
+          <span className="featuredMoneyRate">
+            -1.4
+            <ArrowDownwardIcon />
+          </span>
+        </div>
+        <span className="featuredSub">Compare to last month</span>
+      </div>
+      <div className="featuredItem">
+        <span className="featuredTitle">Revanue</span>
+        <div className="featuredMoneyContainer">
+          <span className="featuredMoney">$2,415</span>
+          <span className="featuredMoneyRate">
+            +2.4
+            <ArrowUpwardIcon />
+          </span>
+        </div>
+        <span className="featuredSub">Compare to last month</span>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedInfo;
