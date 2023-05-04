13eaf60195e9703b51df5e2178ce34ffe82a7162
patryk77k <patryk77.k@gmail.com>
2023-05-04 22:14:15 +0200

    routes fixed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import Sidebar from "./components/sidebar/Sidebar";
 import { Topbar } from "./components/topbar/Topbar";
 import Home from "./pages/home/Home";
+import UserList from "./pages/userList/UserList";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import "./app.css";
 
 function App() {
   return (
     <div>
-      <Topbar />
+      <Routes>
+        <Route path="/" element={<Topbar />} />
+      </Routes>
       <div className="container">
         <Sidebar />
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
     </div>
   );
