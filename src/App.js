c8ba3274aa0aeb22f3634f3509f21bcf801b3b8e
patryk77k <patryk77.k@gmail.com>
2023-05-05 01:46:22 +0200

    product list ready

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Sidebar from "./components/sidebar/Sidebar";
 import { Topbar } from "./components/topbar/Topbar";
+import ProductList from "./pages/productList/ProductList";
 import User from "./pages/user/User";
 import Home from "./pages/home/Home";
 import NewUser from "./pages/newPage/NewUser";
@@ -19,6 +20,9 @@ function App() {
           <Route path="/users" element={<UserList />} />
           <Route path="/users/:userId" element={<User />} />
           <Route path="/newUser" element={<NewUser />} />
+          <Route path="/products" element={<ProductList />} />
+          <Route path="/products/:productId" element={<ProductList />} />
+          <Route path="/newproduct" element={<ProductList />} />
         </Routes>
       </div>
     </div>
